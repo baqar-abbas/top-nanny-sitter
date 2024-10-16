48d7736a5327e7e4a41ff5ecd9de23ed68b3f02c
baqar-abbas <baqar.abbas@hotmail.com>
2024-10-16 06:10:56 +0500

    Setup Tailwind CSS, Created Header component and imported in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import './App.css'
+import Header from './Global components/Header';
 
 function App() {
 
   return (
     <>
-    <h1>
-      Hello Nanny Sitter App
-    </h1>
+    <Header />
+    <div className="mt-32 p-6 bg-gray-100">
+      <h1 className="custom-heading text-center">Welcome to the Top Nanny Sitter Site</h1>
+      <button className="custom-button">Get Started</button>
+      <button className="mt-4 bg-red-500 text-white py-2 px-4 rounded">
+        Tailwind Button
+      </button>
+    </div>
     </>
   )
 }
 
-export default App
+export default App;
